584903f97bdbd3d38804f702ac510ead523c70a0
Aminujjma <72920878+Aminujjma@users.noreply.github.com>
2023-12-21 14:00:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let a
-let time
+let Time
 let date
 setInterval(()=>{
 
 
 const option= { weekday:"long", year:"numeric", month:"short", day:"numeric"};
 a= new Date();
-time= a.getHours() + ":" + a.getMinutes() + ":" +a.getSeconds();
+Time= a.getHours() + ":" + a.getMinutes() + ":" +a.getSeconds();
 date= a.toLocaleDateString(undefined,option);
 document.getElementById("time").innerHTML = time+ " on <br>  "+ date;
 },10)
